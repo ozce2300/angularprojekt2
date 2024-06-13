ce58f0f93039b819f614538e299efed1e7e791b9
Özgur Celik <ozce2300@student.miun.se>
2024-06-13 11:09:35 +0200

    Header klar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { KurserComponent } from './kurser/kurser.component';
+import { LaddakurserComponent } from './laddakurser/laddakurser.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: KurserComponent},
+    {path: "kurser", component:LaddakurserComponent},
+    {path: "**", redirectTo: ""}
+];
