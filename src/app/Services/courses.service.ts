220b940185aeb713bc97b736c5de2a3ca676e984
Özgur Celik <ozce2300@student.miun.se>
2024-06-19 10:36:14 +0200

    Startsida klar med tabell, filter, sort och btn med localstorage

--- src/app/Services/courses.service.ts
+++ src/app/Services/courses.service.ts
@@ -1,15 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Courses } from '../models/courses';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
-  url : string ="https://matdah.github.io/DT208G---Programmering-i-TypeScript/Moment%205%20-%20Projekt/miun_courses.json";
+  private url : string ="https://matdah.github.io/DT208G---Programmering-i-TypeScript/Moment%205%20-%20Projekt/miun_courses.json";
 
   constructor(private http: HttpClient) { }
 
-  getCourses(): Observable
+  getCourses(): Observable<Courses[]>{
+    return this.http.get<Courses[]>(this.url)
+  }
 }
--- src/app/kurser/kurser.component.ts
+++ src/app/kurser/kurser.component.ts
@@ -1,13 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CoursesService } from '../Services/courses.service';
+import { Courses } from '../models/courses';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-kurser',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule
+  ],
   templateUrl: './kurser.component.html',
-  styleUrl: './kurser.component.css'
+  styleUrls: ['./kurser.component.css']
 })
-export class KurserComponent {
-  
+export class KurserComponent{
+  displayedColumns: string[] = ['courseName', 'courseCode', 'points', 'subject', 'subjectCode', 'syllabus', 'addCourse'];
+  dataSource = new MatTableDataSource<Courses>([]);
+  subjects: string[] = [];
+  allCourses: Courses[] = [];
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private coursesService: CoursesService) { }
+
+  ngOnInit() {
+    this.coursesService.getCourses().subscribe(courses => {
+      this.allCourses = courses;
+      this.dataSource.data = courses;
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+      this.subjects = [...new Set(courses.map(course => course.subject))];
+    });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  onSubjectChange(subject: string) {
+    if (subject) {
+      this.dataSource.data = this.allCourses.filter(course => course.subject === subject);
+    } else {
+      this.dataSource.data = this.allCourses;
+    }
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  addCourseToLocalStorage(course: Courses) {
+    let storedCourses = JSON.parse(localStorage.getItem('storedCourses') || '[]');
+    storedCourses.push(course);
+    localStorage.setItem('storedCourses', JSON.stringify(storedCourses));
+  }
 }
